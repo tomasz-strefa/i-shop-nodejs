b32c7c41289b21ea49721695bc6ba284b09e1f8b
Tomasz Gądek <tomasz@eduj.pl>
2020-10-08 16:08:51 +0200

    wysylanie zamowienia

--- repositories/ProductsRepository.js
+++ repositories/ProductsRepository.js
@@ -1,16 +1,12 @@
 var fs = require("fs");
+const e = require("express");
 
 class ProductsRepository {
 
-    products;
+    products = [];
 
     constructor(baseDir) {
-        
-        console.log('dirname = ', baseDir);
-        
         this.products = fs.readFileSync( baseDir + "/entities/" + "products.json", 'utf8');
-
-        console.log('result = ', this.products);
     }
 
     getProducts() {
@@ -18,10 +14,14 @@ class ProductsRepository {
     }
 
     findOne(productId) {
-        const element = this.products.find(el => el.productId == productId);
+
+        const productsArray = JSON.parse(this.products);
+        console.log('products length = ', productsArray.length);
+        const element = productsArray.find(function(el) {
+            return el.productId === productId;
+        });
         console.log('found: ', element);
         return element;
-
     }
 }
 
--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.get('/api/i-shop/products', function (req, res) {
 
 app.post('/api/i-shop/order', function (req, res) {
     const result = checkRequest(req.body);
-    res.sendStatus(result ? 200 : 400);
+    console.log('STATUS === ', result);
+    res.writeHead(result ? 200 : 400);
+    res.end();
 })
 
 var server = app.listen(8081, function () {
@@ -28,14 +30,40 @@ var server = app.listen(8081, function () {
 })
 
 function checkRequest(body) {
-    console.log('body -> ', body);
-    result = false
+    resultValid = false;
+
     body.products.forEach(el => {
-        console.log('el price = ' + el.price + ' body price: ' + body.price);
-        if(el.price == body.price) {
-            result = true;
+
+        if(_isValidProduct(el)) {
+            resultValid = true;
         }
     });
 
-    return result;
+    checkSize = false;
+    counterProducts = 0;
+    body.products.forEach(el => {
+        counterProducts += el.counter;
+    });
+
+    if(counterProducts === body.productsCounter) {
+        checkSize = true;
+    }
+
+    checkPrice = false;
+    sumPrice = 0.0;
+    body.products.forEach(el => {
+        sumPrice += (el.price * el.counter);
+    });
+
+    if(sumPrice === body.productsPrice) {
+        checkPrice = true;
+    }
+
+    return resultValid && checkSize && checkPrice;
+}
+
+function _isValidProduct(productDTO) {
+    var product = productsRepository.findOne(productDTO.productId);
+    
+    return product.price === productDTO.price;
 }
